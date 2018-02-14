76c3fee04afa2572a522e4bb81de1556277f044e
Eddie-CooRo <1300100eddiecooro@gmail.com>
2018-02-14 18:35:08 +0330

    Revert "Revert "dynamic image address""
    This reverts commit 0e8591993a7c15eb2ea6b7793055d33a027b8612.

--- push-notification-dashboard/src/Navbar/Navbar.profile.js
+++ push-notification-dashboard/src/Navbar/Navbar.profile.js
@@ -3,11 +3,12 @@ import './Navbar.profile.css';
 
 class NavbarProfile extends Component {
   render() {
+    const profileImageAddress = "http://" + window.location.host + "/img/img.jpg"
     const stateClass = this.props.isOpen ? 'open' : 'close';
     return (
       <div className={'profile-container ' + stateClass}>
         <div className="profile-image-container">
-          <div className="profile-image" />
+          <div className="profile-image" style={{backgroundImage:`url(${profileImageAddress})`}} />
         </div>
         <div className="profile-details">
           <p className="profile-username">Eddie CooRo</p>
